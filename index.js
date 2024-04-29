6c6b9f09db941631eaced6e52939183a1135153c
Liviadbelao <125027552+Liviadbelao@users.noreply.github.com>
2024-04-29 08:38:09 -0300

    feat: organizando retorno das respostas das tratativas de erro

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'harrypotter',
-    password: 'liv121006',
-    port: 5432,
+    password: 'ds564',
+    port: 7007,
 });
 app.use(express.json());
 
@@ -151,12 +151,16 @@ app.post('/bruxos', async (req, res) => {
 
         let casa_bruxo = ['lufa-lufa', 'corvinal', 'sonserina', 'grifinória']
         let tipo_sangue = ['puro', 'mestiço', 'trouxa'];
+        
 
-        if (!casa_bruxo.includes(casa)) {
-            res.status(401).send({ mensagem: 'casa não existente no mundo de hogwarts' });
+        if(!nome || !idade || !casa_hogwarts || !habilidade || !status_sangue ){
+            res.status(401).send({ mensagem: 'todos os campos, exceto o de patrono, devem ser preenchidos.' });
+        }
+        else if (!casa_bruxo.includes(casa)) {
+            res.status(401).send({ mensagem: 'casa não existente no mundo de hogwarts, ou escrita incorretamente, tente usar as seguintes opções: "lufa-lufa", "corvinal", "sonserina" ou "grifinória"' });
         }
         else if (!tipo_sangue.includes(sangue)) {
-            res.status(401).send({ mensagem: 'status de sangue definido incorretamente' });
+            res.status(401).send({ mensagem: 'status de sangue definido incorretamente, use umas das seguintes opções: "puro", "mestiço" ou "trouxa ' });
         }
         else if (nome.length < 3) {
             res.status(401).send({ mensagem: 'nome inválido' });
@@ -164,7 +168,7 @@ app.post('/bruxos', async (req, res) => {
         else if (idade <= 0) {
             res.status(401).send({ mensagem: 'esse bruxo não tem idade para estudar em hogwarts' });
         }
-    
+      
         else {
             await pool.query('INSERT INTO bruxos (nome, idade, casa_hogwarts, habilidade, status_sangue, patrono) VALUES ($1, $2, $3, $4, $5, $6)', [nome, idade, casa, habilidade, sangue, patrono]);
             res.status(201).send({ mensagem: 'bruxo criado com sucesso' });
@@ -182,14 +186,17 @@ app.post('/varinhas', async (req, res) => {
 
         const data = new Date(data_fabricacao)
 
-        if (comprimento < 10.0) {
+        if(!material || !comprimento || !nucleo || !data_fabricacao){
+            res.status(401).send({ mensagem: 'preencha todos os campos' });   
+        }
+        else if (comprimento < 10.0) {
             res.status(401).send({ mensagem: 'varinha muito pequena' });
         }
         else if (comprimento > 50.0) {
             res.status(401).send({ mensagem: 'varinha muito grande' });
         }
         else if (data > new Date()) {
-            res.status(401).send({ mensagem: 'impossivel inserir uma varinha com a data de fabricação maior que o dia de hoje' });
+            res.status(401).send({ mensagem: 'impossivel inserir uma varinha com a data de fabricação maior que o dia de hoje!' });
         }
         else {
             await pool.query('INSERT INTO varinhas (material, comprimento, nucleo, data_fabricacao) VALUES ($1, $2, $3, $4)', [material, comprimento, nucleo, data_fabricacao]);
@@ -234,7 +241,10 @@ app.put('/bruxos/:id', async (req, res) => {
         let casa_bruxo = ['lufa-lufa', 'corvinal', 'sonserina', 'grifinória']
         let tipo_sangue = ['puro', 'mestiço', 'trouxa'];
 
-        if (!casa_bruxo.includes(casa)) {
+        if(!nome || !idade || !casa_hogwarts || !habilidade || !status_sangue ){
+            res.status(401).send({ mensagem: 'todos os campos, exceto o de patrono, devem ser preenchidos.' });
+        }
+        else if (!casa_bruxo.includes(casa)) {
             res.status(401).send({ mensagem: 'casa não existente no mundo de hogwarts' });
         }
         else if (!tipo_sangue.includes(sangue)) {
@@ -264,7 +274,10 @@ app.put('/varinhas/:id', async (req, res) => {
 
         const data = new Date(data_fabricacao)
 
-        if (comprimento < 10.0) {
+        if(!material || !comprimento || !nucleo || !data_fabricacao){
+            res.status(401).send({ mensagem: 'preencha todos os campos' });   
+        }
+        else if (comprimento < 10.0) {
             res.status(401).send({ mensagem: 'varinha muito pequena' });
         }
         else if (comprimento > 50.0) {
